f05e5929d9c727b6f3fae1c3a83223010934d784
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2020-05-09 21:12:14 -0400

    user data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Title from './Components/Title';
+import List from './Components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Title title = {'To Do'}/>
+        <List tasks = {['1','2']}/>
+        <List tasks = {['3','4']}/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/info.js
@@ -0,0 +1,91 @@
+/* Sitewide variable text */
+
+export default
+  {
+    personal: {
+      name: 'Lawrence Castillo',
+      email: 'Lawrence.g.castillo@gmail.com',
+      location: 'New York, NY',
+      headline: 'Hey I\'m a Software Developer in NYC',
+      image: 'https://avatars1.githubusercontent.com/u/33098457?s=460&u=65f0d2c78bc9870e13e0b2147cc8f533a175d497&v=4',
+      about: 'From carving woodblocks and making paper, to electroplating silver to copper for Daguerreotypes, I like to get my hands dirty when I\'m not working on code.'
+    },
+    work: [
+      {
+        id: 1,
+        company: 'Threshold.co',
+        logo: '',
+        url: '',
+        location: '',
+        timeframe: 'May 2020 - Current',
+        position: 'web dev (intern)',
+        description: 'Support app launch. Progressive web app (PWA) with Preact, Redux...'
+      },
+      {
+        id: 2,
+        company: 'Threshold.co',
+        logo: '',
+        url: '',
+        location: '',
+        timeframe: 'May 2020 - Current',
+        position: 'web dev (intern)',
+        description: 'Support app launch. Progressive web app (PWA) with Preact, Redux...'
+      }
+    ],
+    projects: [
+      {
+        id: 1,
+        title: '',
+        logo: '',
+        url: '',
+        stack: '',
+        description: ''
+      },
+      {
+        id: 2,
+        title: '',
+        logo: '',
+        url: '',
+        stack: '',
+        description: ''
+      }
+    ],
+    education: [
+      {
+        id: 1,
+        institution: '',
+        logo: '',
+        url: '',
+        timeframe: 'May 2020 - Current',
+        degree: '',
+        description: ''
+      },
+      {
+        id: 2,
+        institution: '',
+        logo: '',
+        url: '',
+        timeframe: 'May 2020 - Current',
+        degree: '',
+        description: ''
+      }
+    ],
+    skills: [
+      'C++', 'JavaScript', 'MERN', 'LAMP'
+    ],
+    interests: [
+      'Making', 'soldering'
+    ],
+    social: [
+      {
+        id: 1,
+        logo: '',
+        url: ''
+      },
+      {
+        id: 2,
+        logo: '',
+        url: ''
+      }
+    ]
+  }
